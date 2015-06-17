f45c97c2983bd39f8c636498b1d7db886d60e627
tcarver <tjc29@duffield>
2015-06-17 11:15:36 +0100

    loader for elastic indices

---
+++ data_pipeline/load.py
@@ -0,0 +1,33 @@
+''' Loaders '''
+import os
+import logging
+from .utils import IniParser
+from django.core.management import call_command
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+class IndexLoad(IniParser):
+    ''' Load into Elastic '''
+
+    def load(self, ini_file, dir_path, sections=None):
+        ''' Download data defined in the ini file. '''
+        return self.process_sections(self.read_ini(ini_file), dir_path, sections)
+
+    def process_section(self, section_name, section_dir_name, base_dir_path, dir_path='.', section=None):
+        ''' Overrides L{IniParser.process_section} to process a section
+        in the config file '''
+        if 'output' not in section:
+            return
+        stage_file = os.path.join(base_dir_path, 'STAGE', section_dir_name,
+                                  section['output'] + '.json')
+
+        if 'indexType' in section:
+            indexType = section['indexType']
+        else:
+            parts = section_name.rsplit(':', 1)
+            indexType = parts[1].lower()
+
+        logger.debug('Loading: '+stage_file + ' into ' + section['index'])
+        call_command('index_search', indexType=indexType, indexJson=stage_file, indexName=section['index'])
