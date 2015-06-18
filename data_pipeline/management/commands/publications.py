527c851ec1911a01f27883dfa70d1b0fe675b7ab
tcarver <tjc29@duffield>
2015-06-18 12:04:45 +0100

    add steps option

--- data_pipeline/management/commands/publications.py
+++ data_pipeline/management/commands/publications.py
@@ -1,5 +1,5 @@
 ''' Command line tool to manage downloads. '''
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from data_pipeline.download import Download
 from data_pipeline.load import IndexLoad
 
@@ -11,22 +11,22 @@ class Command(BaseCommand):
         parser.add_argument('--dir',
                             dest='dir',
                             metavar="/download_path/",
-                            help='Directory to store downloads.')
+                            help='Directory to store downloads.', required=True)
         parser.add_argument('--ini',
                             dest='ini',
                             help='Input file defining downloads.')
         parser.add_argument('--sections',
                             dest='sections',
                             help='Comma separated section names (e.g. BANDS) to download [default: all].')
-#         parser.add_argument('--download',
-#                             dest='download',
-#                             help='Download step',
-#                             action="store_true")
+        parser.add_argument('--steps',
+                            dest='steps',
+                            help='Steps to run [download load]',
+                            nargs='+', required=True)
 
     def handle(self, *args, **options):
 
-        if not options['dir']:
-            raise CommandError('--dir parameter not provided')
-        if Download().download_ini(options['ini'], options['dir'], options['sections']):
-            self.stdout.write("DOWNLOAD COMPLETE")
-        IndexLoad().load(options['ini'], options['dir'], options['sections'])
+        if 'download' in options['steps']:
+            if Download().download_ini(options['ini'], options['dir'], options['sections']):
+                self.stdout.write("DOWNLOAD COMPLETE")
+        if 'load' in options['steps']:
+            IndexLoad().load(options['ini'], options['dir'], options['sections'])
