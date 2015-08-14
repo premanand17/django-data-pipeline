eeede6a79d542556cec67d60ae65426fc99e9d03
tcarver <tjc29@duffield>
2015-08-14 11:15:03 +0100

    add gene history loading

--- data_pipeline/management/commands/pipeline.py
+++ data_pipeline/management/commands/pipeline.py
@@ -22,6 +22,9 @@ class Command(BaseCommand):
     Update gene suggester weighting:
     python criteria_suggester.py gene
 
+    Gene History:
+    ./manage.py pipeline --dir tmp --ini download.ini --sections GENE_HISTORY --steps download load
+
     Marker:
     ./manage.py pipeline --dir /dbSNP/human/144/ --ini download.ini --sections DBSNP --steps download load
     ./manage.py pipeline --dir /dbSNP/human/144/ --ini download.ini --sections RSMERGEARCH --steps download load
