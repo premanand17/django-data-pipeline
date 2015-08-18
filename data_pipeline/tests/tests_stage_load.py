0e6a207e6a8f2ef54d9ee38035b0e12cf57d4d00
tcarver <tjc29@duffield>
2015-08-18 09:37:19 +0100

    add GENE2ENSEMBL to gene pipeline test

--- data_pipeline/tests/tests_stage_load.py
+++ data_pipeline/tests/tests_stage_load.py
@@ -7,18 +7,33 @@ import shutil
 from data_pipeline.utils import IniParser
 from elastic.elastic_settings import ElasticSettings
 import requests
-from elastic.search import Search
+from elastic.search import Search, ElasticQuery
 from data_pipeline.helper.gene import Gene
 
-INI_FILE = os.path.join(os.path.dirname(__file__), 'test_download.ini')
+IDX_SUFFIX = ElasticSettings.getattr('TEST')
+MY_INI_FILE = os.path.join(os.path.dirname(__file__), IDX_SUFFIX + '_test_download.ini')
 TEST_DATA_DIR = os.path.dirname(data_pipeline.__file__) + '/tests/data'
-INI_CONFIG = IniParser().read_ini(INI_FILE)
+
+
+def setUpModule():
+    ''' Change ini config (MY_INI_FILE) to use the test suffix when
+    creating pipeline indices. '''
+    ini_file = os.path.join(os.path.dirname(__file__), 'test_download.ini')
+    if os.path.isfile(MY_INI_FILE):
+        return
+
+    with open(MY_INI_FILE, 'w') as new_file:
+        with open(ini_file) as old_file:
+            for line in old_file:
+                new_file.write(line.replace('auto_tests', IDX_SUFFIX))
 
 
 def tearDownModule():
     shutil.rmtree(TEST_DATA_DIR + '/STAGE')
     # remove index created
+    INI_CONFIG = IniParser().read_ini(MY_INI_FILE)
     requests.delete(ElasticSettings.url() + '/' + INI_CONFIG['GENE_HISTORY']['index'])
+    os.remove(MY_INI_FILE)
 
 
 class StageTest(TestCase):
@@ -26,7 +41,7 @@ class StageTest(TestCase):
     def test_gene_interaction_parse(self):
         ''' Test ini file downloads. '''
         call_command('pipeline', '--steps', 'stage', sections='INTACT',
-                     dir=TEST_DATA_DIR, ini=INI_FILE)
+                     dir=TEST_DATA_DIR, ini=MY_INI_FILE)
 
         self.assertTrue(os.path.exists(TEST_DATA_DIR + '/STAGE/INTACT'))
         self.assertTrue(os.path.isfile(TEST_DATA_DIR + '/STAGE/INTACT/intact.zip.out'))
@@ -36,10 +51,14 @@ class StageTest(TestCase):
 
 class LoadTest(TestCase):
 
-    def test_ensembl_gene(self):
-        ''' Test ensembl GTF loading. '''
+    def test_gene_pipeline(self):
+        ''' Test gene pipeline. '''
+
+        ''' 1. Test ensembl GTF loading. '''
         call_command('pipeline', '--steps', 'stage', 'load', sections='ENSEMBL_GENE_GTF',
-                     dir=TEST_DATA_DIR, ini=INI_FILE)
+                     dir=TEST_DATA_DIR, ini=MY_INI_FILE)
+
+        INI_CONFIG = IniParser().read_ini(MY_INI_FILE)
         idx = INI_CONFIG['ENSEMBL_GENE_GTF']['index']
         idx_type = INI_CONFIG['ENSEMBL_GENE_GTF']['index_type']
         elastic = Search(idx=idx, idx_type=idx_type)
@@ -49,11 +68,22 @@ class LoadTest(TestCase):
         map_props = Gene.gene_mapping(idx, idx_type, test_mode=True).mapping_properties
         self._cmpMappings(elastic.get_mapping()[idx]['mappings'], map_props, idx_type)
 
+        ''' 2. Test adding entrez ID to documents '''
+        call_command('pipeline', '--steps', 'load', sections='GENE2ENSEMBL',
+                     dir=TEST_DATA_DIR, ini=MY_INI_FILE)
+        Search.index_refresh(idx)
+        query = ElasticQuery.query_string("PTPN22", fields=["symbol"])
+        elastic = Search(query, idx=idx)
+        docs = elastic.search().docs
+        self.assertEqual(len(docs), 1)
+        self.assertTrue('entrez' in getattr(docs[0], "dbxrefs"))
+
     def test_gene_history_loader(self):
         ''' Test the gene history loading. '''
         call_command('pipeline', '--steps', 'load', sections='GENE_HISTORY',
-                     dir=TEST_DATA_DIR, ini=INI_FILE)
+                     dir=TEST_DATA_DIR, ini=MY_INI_FILE)
 
+        INI_CONFIG = IniParser().read_ini(MY_INI_FILE)
         idx = INI_CONFIG['GENE_HISTORY']['index']
         idx_type = INI_CONFIG['GENE_HISTORY']['index_type']
         elastic = Search(idx=idx, idx_type=idx_type)
@@ -71,4 +101,5 @@ class LoadTest(TestCase):
         sorted_keys.sort()
         for k in sorted_keys:
             self.assertTrue(k in map2, k)
-            self.assertEqual(map1[k]['type'], map2[k]['type'], k)
+            if 'type' in map1[k]:
+                self.assertEqual(map1[k]['type'], map2[k]['type'], k)
