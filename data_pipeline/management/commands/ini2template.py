7684387bac017aee65a7b7b4ecc32a24b5d5161d
tcarver <tim.carver@cimr.cam.ac.uk>
2016-03-31 17:17:52 +0100

    in2template generate versions

--- data_pipeline/management/commands/ini2template.py
+++ data_pipeline/management/commands/ini2template.py
@@ -1,36 +1,43 @@
 ''' Command line tool to get download sources and versions. '''
-from django.core.management.base import BaseCommand, CommandError
+import datetime
 import logging
+from django.core.management.base import BaseCommand, CommandError
 from data_pipeline.utils import IniParser
-import json
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-class Ini2Json(IniParser):
-
-    res = {}
+class Ini2Template(IniParser):
 
     def convert(self, ini_file):
         ''' Covert the ini file. '''
         self.process_sections(self.read_ini(ini_file), ".")
-        print(json.dumps(Ini2Json.res))
+        # print(json.dumps(Ini2Json.res))
 
     def process_section(self, fname, section_dir_name, base_dir_path,
                         dir_path='.', section=None, stage='download', config=None):
         ''' Overrides L{IniParser.process_section} to process a section
         in the config file '''
-        s = Ini2Json.res
-
         d = ''
+
         if 'location' in section:
             d += section['location']
             if 'files' in section:
                 d += section['files']
 
         if d != '':
-            s[section.name] = {'download': d}
+            name = section['name'] if 'name' in section else section.name
+            print('<div class="row">')
+            print('<div class="col-md-2">' + name +
+                  '</div><div class="col-md-8">' + d, end="</div>")
+
+            if 'version' in section:
+                print('<div class="col-md-2">'+section['version'], end="</div>")
+            else:
+                print('<div class="col-md-2">'+datetime.datetime.now().strftime("%d-%m-%y"), end="</div>")
+            print("</div>")
 
 
 class Command(BaseCommand):
@@ -42,7 +49,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logger.debug(options)
-        ini = Ini2Json()
+        ini = Ini2Template()
         if options['ini']:
             if options['ini']:
                 ini.convert(options['ini'])
